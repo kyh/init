705df1b7ee1196f8720435c6e79d876dd783e932
Kaiyu Hsu <im.kaiyu@gmail.com>
2025-02-09 18:38:23 -0800

    fix meteor

--- apps/nextjs/src/app/(marketing)/_components/meteor.tsx
+++ apps/nextjs/src/app/(marketing)/_components/meteor.tsx
@@ -22,18 +22,18 @@ export const Meteors = ({ top = -5, number = 20 }: MeteorsProps) => {
   }, [top, number]);
 
   return (
-    <>
+    <div className="pointer-events-none absolute inset-0 h-full w-full rotate-y-180 overflow-hidden">
       {[...meteorStyles].map((style, idx) => (
         <span
           key={idx}
           className={cn(
-            "pointer-events-none absolute left-1/2 top-1/2 h-0.5 w-0.5 rotate-[215deg] animate-meteor rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10]",
+            "animate-meteor pointer-events-none absolute top-1/2 left-1/2 h-0.5 w-0.5 rotate-[2deg] rounded-[9999px] bg-slate-500 shadow-[0_0_0_1px_#ffffff10]",
           )}
           style={style}
         >
-          <div className="pointer-events-none absolute top-1/2 -z-10 h-[1px] w-[50px] -translate-y-1/2 bg-gradient-to-r from-slate-500 to-transparent" />
+          <span className="pointer-events-none absolute top-1/2 -z-10 h-[1px] w-[50px] -translate-y-1/2 bg-gradient-to-r from-slate-500 to-transparent" />
         </span>
       ))}
-    </>
+    </div>
   );
 };
--- apps/nextjs/src/app/(marketing)/page.tsx
+++ apps/nextjs/src/app/(marketing)/page.tsx
@@ -4,55 +4,46 @@ import { WaitlistForm } from "@/app/(marketing)/_components/waitlist-form";
 const Page = () => {
   return (
     <>
-      <section>
-        <div className="border-border relative mx-auto max-w-7xl border-x border-b p-8 lg:py-32">
-          <div className="max-w-2xl">
-            <span className="text-muted-foreground font-light">
-              Accelerate building modern applications
-            </span>
-            <h1 className="text-secondary-foreground mt-6 text-2xl font-light">
-              The most comprehensive boilerplate to build, launch, and scale
-              your next project.
-            </h1>
-            <WaitlistForm />
-          </div>
-          <div
-            className="pointer-events-none absolute inset-0 h-full w-full overflow-hidden"
-            style={{ transform: `rotateY(180deg)` }}
-          >
-            <Meteors number={15} />
-          </div>
+      <section className="border-border relative mx-auto max-w-7xl border-x border-b p-8 lg:py-32">
+        <div className="max-w-2xl">
+          <span className="text-muted-foreground font-light">
+            Accelerate building modern applications
+          </span>
+          <h1 className="text-secondary-foreground mt-6 text-2xl font-light">
+            The most comprehensive boilerplate to build, launch, and scale your
+            next project.
+          </h1>
+          <WaitlistForm />
         </div>
+        <Meteors />
       </section>
-      <section>
-        <div className="border-border mx-auto max-w-7xl border-x border-b">
-          <div className="divide-gray text-secondary-foreground grid grid-cols-1 md:grid-cols-3 md:divide-x">
-            <div className="flex h-full flex-col gap-6 p-8">
-              <div>Build</div>
-              <p className="text-muted-foreground">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Inventore autem alias doloribus quod consequuntur hic, dolorum
-                at itaque iusto porro minima quia nemo reiciendis natus, dolores
-                quam, voluptatum est explicabo?
-              </p>
-            </div>
-            <div className="flex h-full flex-col gap-6 p-8">
-              <div>Launch</div>
-              <p className="text-muted-foreground">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Inventore autem alias doloribus quod consequuntur hic, dolorum
-                at itaque iusto porro minima quia nemo reiciendis natus, dolores
-                quam, voluptatum est explicabo
-              </p>
-            </div>
-            <div className="flex h-full flex-col gap-6 p-8">
-              <div>Scale</div>
-              <p className="text-muted-foreground">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Inventore autem alias doloribus quod consequuntur hic, dolorum
-                at itaque iusto porro minima quia nemo reiciendis natus
-              </p>
-            </div>
+      <section className="border-border mx-auto max-w-7xl border-x border-b">
+        <div className="divide-gray text-secondary-foreground grid grid-cols-1 md:grid-cols-3 md:divide-x">
+          <div className="flex h-full flex-col gap-6 p-8">
+            <div>Build</div>
+            <p className="text-muted-foreground">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+              autem alias doloribus quod consequuntur hic, dolorum at itaque
+              iusto porro minima quia nemo reiciendis natus, dolores quam,
+              voluptatum est explicabo?
+            </p>
+          </div>
+          <div className="flex h-full flex-col gap-6 p-8">
+            <div>Launch</div>
+            <p className="text-muted-foreground">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+              autem alias doloribus quod consequuntur hic, dolorum at itaque
+              iusto porro minima quia nemo reiciendis natus, dolores quam,
+              voluptatum est explicabo
+            </p>
+          </div>
+          <div className="flex h-full flex-col gap-6 p-8">
+            <div>Scale</div>
+            <p className="text-muted-foreground">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+              autem alias doloribus quod consequuntur hic, dolorum at itaque
+              iusto porro minima quia nemo reiciendis natus
+            </p>
           </div>
         </div>
       </section>
