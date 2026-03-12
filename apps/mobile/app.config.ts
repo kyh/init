1a0693ace998771682a55ce04ae450bb8faf1bdd
Kaiyu Hsu <im.kaiyu@gmail.com>
2026-03-12 15:16:19 -0700

    update packages

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -28,7 +28,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       foregroundImage: "./assets/icon-light.png",
       backgroundColor: "#1F104A",
     },
-    edgeToEdgeEnabled: true,
   },
   // extra: {
   //   eas: {
